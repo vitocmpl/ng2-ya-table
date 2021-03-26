f66884321dd1ad6fe02de0989130fe1d51b47857
Vito Campanelli <vito.campanelli@wolterskluwer.com>
2021-03-26 14:36:25 +0100

    clean

--- src/ng2-ya-table-directives.ts
+++
@@ -1,5 +0,0 @@
-import { Ng2YaTableComponent } from './ng2-ya-table/ng2-ya-table.component';
-import { Ng2YaTableSortingDirective } from './ng2-ya-table/ng2-ya-table-sorting.directive';
-import { Ng2YaTableFilteringDefaultComponent } from './ng2-ya-table/ng2-ya-table-filtering-default.component';
-import { Ng2YaTableFilteringListComponent } from './ng2-ya-table/ng2-ya-table-filtering-list.component';
-export const NG2_YA_TABLE_DIRECTIVES = [Ng2YaTableComponent, Ng2YaTableFilteringDefaultComponent, Ng2YaTableFilteringListComponent, Ng2YaTableSortingDirective];
