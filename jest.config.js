2b54efdea5ce8f372c4a3363006e06b07021521e
Vito Campanelli <vitocmpl@gmail.com>
2022-02-25 19:25:30 +0100

    @angular-builders/jest configuration

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,6 @@ module.exports = {
   preset: 'jest-preset-angular',
   roots: ['<rootDir>/src/'],
   testMatch: ['**/+(*.)+(spec).+(ts)'],
-  setupFilesAfterEnv: ['<rootDir>/src/test.ts'],
   collectCoverage: true,
   coverageReporters: ['html'],
   coverageDirectory: 'coverage/my-app',
