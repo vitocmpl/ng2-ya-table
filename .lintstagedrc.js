49c80c6f3ba0b0d351a7c465ff20f9b1cfa6aa28
Vito Campanelli <vitocmpl@gmail.com>
2022-03-04 09:15:48 +0100

    husky

---
+++ .lintstagedrc.js
@@ -0,0 +1,18 @@
+const { ESLint } = require('eslint');
+const removeIgnoredFiles = async (files) => {
+  const eslint = new ESLint();
+  const isIgnored = await Promise.all(
+    files.map((file) => {
+      return eslint.isPathIgnored(file);
+    })
+  );
+  const filteredFiles = files.filter((_, i) => !isIgnored[i]);
+  return filteredFiles.join(' ');
+};
+
+module.exports = {
+  '{src,project}/**/*.{ts,html,scss}': async (files) => {
+    const filesToLint = await removeIgnoredFiles(files);
+    return [`npx prettier --write ${filesToLint}`, `npx eslint ${filesToLint}`];
+  }
+};
